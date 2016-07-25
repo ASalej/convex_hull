334495399bb0564dee17798d09cd708a111f6505
ASalej <ale-salej@mail.ru>
2016-07-25 16:14:38 +0000

    clean circle class

--- js/models/Circle.js
+++ js/models/Circle.js
@@ -5,13 +5,16 @@ class Circle {
         this.radius = radius;
     }
     
-    isOnCircle(x, y) {
+    isPointOnCircle(x, y) {
+        if(x instanceof Point) {
+            y = x.y;
+            x = x.x;
+        }
         if ((x - this.xCenter) * (x - this.xCenter) + 
             (y - this.yCenter) * (y - this.yCenter) === this.radius) {
                return true;
         } else {
             return false;
         }
-           
     }
 }
