0a813fbfec3acd47457dfb8446df2c64c3b342a3
ASalej <ale-salej@mail.ru>
2016-07-25 16:11:02 +0000

    add graph functional

--- js/models/Graph.js
+++ js/models/Graph.js
@@ -7,6 +7,15 @@ class Graph {
         this.graph.push([point1, point2]);
     }
     
+    findVertex(point) {
+        for(let i = 0; i < this.vertices.length; i++) {
+            if(this.vertices[i].x === point.x && this.vertices[i].y === point.y) {
+                return i;
+            }    
+        }
+        return -1;
+    }
+    
     findEdge(point1, point2) {
         for(let i = 0; i < this.graph.length; i++) {
             if(this.graph[i][0] === point1) {
@@ -29,6 +38,19 @@ class Graph {
                 temp_graph = (this.graph.slice(0, index)).concat(this.graph.slice(index + 1));  
             this.graph = temp_graph;
         }
+    }
+    
+    getVertices() {
+        var vertices = [];
+        for (let i = 0; i < this.graph.length; i++) {
+            if(vertices.indexOf(this.graph[i][0]) === -1) {
+                vertices.push(this.graph[i][0]);
+            }
+            if(vertices.indexOf(this.graph[i][1]) === -1) {
+                vertices.push(this.graph[i][1]);
+            }
+        }
+        return vertices;
     }
     
     // returns array of incident points
